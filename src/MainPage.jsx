e88691b15a1f6984aecd9aa2426dbc8a459068e2
nanako0603 <nnky0603@gmail.com>
2020-10-22 19:50:26 +0900

    Project's Initial Commit

---
+++ src/MainPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Toolbar, Page, Button} from 'react-onsenui';
+import SecondPage from './SecondPage'
+export default class MainPage extends React.Component {
+pushPage() {
+this.props.navigator.pushPage({component: SecondPage, props: {value:1}});
+}
+renderToolbar() {
+return (
+<Toolbar>
+<div className="center">Navigator</div>
+</Toolbar>
+);
+}
+render() {
+return (
+<Page renderToolbar={this.renderToolbar}>
+<p style={{textAlign: 'center'}}>
+<Button onClick={this.pushPage.bind(this)}>Push page</Button>
+</p>
+</Page>
+);
+}
+}
